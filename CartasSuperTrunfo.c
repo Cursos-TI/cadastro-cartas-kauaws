f08b87b2a6827bf1125c2f7d3ae0aa557f338f2e
kauaws <kauahenrysandra@gmail.com>
2026-06-11 23:10:49 +0000

    Acresentei mais variáveis.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,16 +6,32 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado 
-  char código da carta
-  char nome da cidade
-  int população
-  float área
-  float pib
-  int número de pontos turísticos
+  // CARTA 1
+ 
+  char estado1 [25];
+  char código1 [25];
+  char nome da cidade1 [25];
+  int população1;
+  float área1;
+  float pib1;
+  int número de pontos turísticos1;
+
+  // CARTA 2
   
+  char estado1 [10];
+  char código1 [10];
+  char nome da cidade1 [25];
+  int população1;
+  float área1;
+  float pib1;
+  int número de pontos turísticos1;
+
   // Área para entrada de dados
 
+  // CARTA 1
+ 
+  printf("Estado:\n");
+  scanf("%s", estado1);
   // Área para exibição dos dados da cidade
 
 return 0;
